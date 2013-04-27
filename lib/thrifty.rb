16c5c369713a9db4433f88381dd83bb45b6fdcec
Greg Brockman <gdb@gregbrockman.com>
2013-04-27 16:49:10 -0700

    Add proof-of-concept thrift runner

--- lib/thrifty.rb
+++ lib/thrifty.rb
@@ -1,5 +1,31 @@
-require "thrifty/version"
+require 'thrifty/version'
+require 'rubysh'
+
+require 'thrift'
 
 module Thrifty
-  # Your code goes here...
+  def self.cache_directory=(cache_directory)
+    @cache_directory = cache_directory
+  end
+
+  def self.register(thrift_file)
+    raise "No cache defined" unless @cache_directory
+    code_directory = @cache_directory
+    Rubysh('thrift', '--gen', 'rb', '-out', code_directory, thrift_file).check_call
+  end
+
+  def self.require(generated)
+    raise "No cache defined" unless @cache_directory
+    code_directory = @cache_directory
+    $:.unshift(code_directory)
+
+    begin
+      # Global require
+      super(generated)
+    ensure
+      # Not sure what to do if someone changed $: in the
+      # meanwhile.
+      $:.shift if $:.first == code_directory
+    end
+  end
 end
