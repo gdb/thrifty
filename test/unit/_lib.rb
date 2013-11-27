fad49ee599414fbb32c2a40e064d0648da7d7b87
Greg Brockman <gdb@gregbrockman.com>
2013-11-27 12:09:54 -0800

    Add test boilerplate

---
+++ test/unit/_lib.rb
@@ -0,0 +1,12 @@
+require File.expand_path('../../_lib', __FILE__)
+
+module Critic::Unit
+  module Stubs
+  end
+
+  class Test < Critic::Test
+    include Stubs
+  end
+end
+
+MiniTest::Unit.runner = MiniTest::Unit.new
