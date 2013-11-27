0dbe6ca98dd19d61f0899e823ec4e87dac141a7a
Greg Brockman <gdb@gregbrockman.com>
2013-11-27 15:04:57 -0800

    Reindent file

--- lib/thrifty/thrift_file.rb
+++ lib/thrifty/thrift_file.rb
@@ -2,96 +2,94 @@ require 'digest/sha1'
 require 'fileutils'
 require 'rubysh'
 
-module Thrifty
-  class ThriftFile
-    include Chalk::Log
+class Thrifty::ThriftFile
+  include Chalk::Log
 
-    attr_reader :thrift_file, :options
+  attr_reader :thrift_file, :options
 
-    def initialize(thrift_file, options={})
-      @thrift_file = thrift_file
-      @options = options
+  def initialize(thrift_file, options={})
+    @thrift_file = thrift_file
+    @options = options
+  end
+
+  def path
+    path = @thrift_file
+
+    if relative_to = @options[:relative_to]
+      path = File.expand_path(path, File.join(relative_to, '..'))
     end
 
-    def path
-      path = @thrift_file
+    path
+  end
 
-      if relative_to = @options[:relative_to]
-        path = File.expand_path(path, File.join(relative_to, '..'))
-      end
+  def defines?(generated_file)
+    compile_once
+    File.exists?(File.join(build_directory, generated_file + '.rb'))
+  end
 
-      path
-    end
+  def require(generated_file)
+    compile_once
+    $:.unshift(build_directory)
 
-    def defines?(generated_file)
-      compile_once
-      File.exists?(File.join(build_directory, generated_file + '.rb'))
-    end
-
-    def require(generated_file)
-      compile_once
-      $:.unshift(build_directory)
-
-      begin
-        log.info('Requiring', file: generated_file, idl: thrift_file)
-        # Global require
-        super(generated_file)
-      ensure
-        # Not sure what to do if someone changed $: in the
-        # meanwhile. Could happen due a signal handler or something
-        # crazy like that.
-        if $:.first == build_directory
-          $:.shift
-        else
-          log.error('Unexpected first element in load path; not removing', build_directory: build_directory, load_path: $:.inspect)
-        end
+    begin
+      log.info('Requiring', file: generated_file, idl: thrift_file)
+      # Global require
+      super(generated_file)
+    ensure
+      # Not sure what to do if someone changed $: in the
+      # meanwhile. Could happen due a signal handler or something
+      # crazy like that.
+      if $:.first == build_directory
+        $:.shift
+      else
+        log.error('Unexpected first element in load path; not removing', build_directory: build_directory, load_path: $:.inspect)
       end
     end
+  end
 
-    def compile_once
-      with_cache do
-        FileUtils.mkdir_p(build_directory)
-        Rubysh('thrift', '--gen', 'rb', '-out', build_directory, thrift_file).check_call
-      end
+  def compile_once
+    with_cache do
+      FileUtils.mkdir_p(build_directory)
+      Rubysh('thrift', '--gen', 'rb', '-out', build_directory, thrift_file).check_call
+    end
+  end
+
+  private
+
+  def with_cache(&blk)
+    raise "No such Thrift file #{thrift_file.inspect}" unless File.exists?(thrift_file)
+
+    # There's obviously a race if someone changes the file before
+    # the compiler runs, so we capture the sha1 up front so it'll at
+    # least be more likely to falsely invalidate the cache.
+    new_version = Digest::SHA1.file(thrift_file).to_s
+    cached_version = File.read(version_file) if File.exists?(version_file)
+
+    # cached_version will be nil if the cache doesn't exist, so always miss.
+    if new_version == cached_version
+      log.debug("Using cached version")
+      return
     end
 
-    private
+    log.info('Compiling thrift file', file: thrift_file, sha1: new_version, build_directory: build_directory)
+    blk.call
 
-    def with_cache(&blk)
-      raise "No such Thrift file #{thrift_file.inspect}" unless File.exists?(thrift_file)
+    File.open(version_file, 'w') {|f| f.write(new_version)}
+  end
 
-      # There's obviously a race if someone changes the file before
-      # the compiler runs, so we capture the sha1 up front so it'll at
-      # least be more likely to falsely invalidate the cache.
-      new_version = Digest::SHA1.file(thrift_file).to_s
-      cached_version = File.read(version_file) if File.exists?(version_file)
+  def version_file
+    File.join(build_directory, 'VERSION')
+  end
 
-      # cached_version will be nil if the cache doesn't exist, so always miss.
-      if new_version == cached_version
-        log.debug("Using cached version")
-        return
-      end
+  def build_directory
+    # Use the basename for informational purposes, and the SHA1 for
+    # avoid collisions.
+    base = File.basename(thrift_file)
+    sha1 = Digest::SHA1.hexdigest(thrift_file)
+    File.join(basedir, "#{base}-#{sha1}")
+  end
 
-      log.info('Compiling thrift file', file: thrift_file, sha1: new_version, build_directory: build_directory)
-      blk.call
-
-      File.open(version_file, 'w') {|f| f.write(new_version)}
-    end
-
-    def version_file
-      File.join(build_directory, 'VERSION')
-    end
-
-    def build_directory
-      # Use the basename for informational purposes, and the SHA1 for
-      # avoid collisions.
-      base = File.basename(thrift_file)
-      sha1 = Digest::SHA1.hexdigest(thrift_file)
-      File.join(basedir, "#{base}-#{sha1}")
-    end
-
-    def basedir
-      Thrifty.basedir
-    end
+  def basedir
+    Thrifty.basedir
   end
 end
